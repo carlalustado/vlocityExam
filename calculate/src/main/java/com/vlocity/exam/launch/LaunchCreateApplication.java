8d418b4cb8386ce25527f5a5a8970e980b462435
Carla Lustado <carla.q.lustado@gmail.com>
2019-09-15 21:03:45 +0800

    updates

--- calculate/src/main/java/com/vlocity/exam/launch/LaunchCreateApplication.java
+++ calculate/src/main/java/com/vlocity/exam/launch/LaunchCreateApplication.java
@@ -1,5 +1,26 @@
 package com.vlocity.exam.launch;
 
-public class LaunchApplication {
+import java.util.Scanner;
 
+import com.vlocity.exam.schedule.ScheduleProjectPlan;
+
+public class LaunchCreateApplication {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Project Scheduler!");
+		System.out.println("Please input the following to create a project: ");
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Project Name: ");
+		String projName = input.next();
+		System.out.print("Project Start date (MM-dd-yyyy): ");
+		String projStartDate = input.next();
+		
+		System.out.print("Project End date (MM-dd-yyyy): ");
+		String projEndDate = input.next();
+		
+		ScheduleProjectPlan schedProjPlan = new ScheduleProjectPlan();
+		schedProjPlan.createProject(projName, projStartDate, projEndDate);
+	}
 }
