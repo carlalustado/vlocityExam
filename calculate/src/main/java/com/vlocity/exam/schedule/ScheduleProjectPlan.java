8f4c00785012380dab543b23c4a6bbc542fb969d
Carla Lustado <carla.q.lustado@gmail.com>
2019-09-15 18:22:22 +0800

    updates

--- calculate/src/main/java/com/vlocity/exam/schedule/ScheduleProjectPlan.java
+++ calculate/src/main/java/com/vlocity/exam/schedule/ScheduleProjectPlan.java
@@ -1,10 +1,16 @@
 package com.vlocity.exam.schedule;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import com.vlocity.exam.model.Project;
+import com.vlocity.exam.model.Tasks;
+import com.vlocity.exam.model.TasksEnum;
 
 public class ScheduleProjectPlan {
 
@@ -13,14 +19,29 @@ public class ScheduleProjectPlan {
 	public void createSchedule(Project project) {
 		LOGGER.info("createSchedule starting...");
 		
-		
-	}
-	
-	private void validateParams(Project project) {
-		if (project.getProjectID() != null && StringUtils.isNotBlank(project.getProjectName())) {
-			
+		if (project != null) {
+			if (project.getProjectID() != null && StringUtils.isNotBlank(project.getProjectName())) {
+				if (CollectionUtils.isNotEmpty(project.getTasks())) {
+					for (Tasks task : project.getTasks()) {
+						
+					}
+				} else {
+					List<Tasks> tasks = new ArrayList<Tasks>();
+					
+					Tasks task = new Tasks();
+					task.setTaskId(1);
+					task.setTaskName("MOBILIZATION");
+					task.setStatus("");
+					task.setIsDependent(false);
+					task.setDuration(TasksEnum.MOBILIZATION);
+					task.setEndDate(endDate);
+					task.setStartDate(startDate);
+					
+				}
+			}
+		} else {
+			// create new project
 		}
 	}
-	
 	
 }
