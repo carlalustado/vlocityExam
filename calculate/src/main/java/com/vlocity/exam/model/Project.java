56620a47d257a097d465892a729f9a9387bf6abd
Carla Lustado <carla.q.lustado@gmail.com>
2019-09-15 18:02:39 +0800

    updates

--- calculate/src/main/java/com/vlocity/exam/model/Project.java
+++ calculate/src/main/java/com/vlocity/exam/model/Project.java
@@ -1,5 +1,78 @@
 package com.vlocity.exam.model;
 
-public class Projects {
+import java.util.Date;
+import java.util.List;
 
+public class Project {
+
+	private String projectName;
+	private Integer projectID;
+	private Date startDate;
+	private Date endDate;
+	private String description;
+	private String status;
+	private List<Tasks> tasks;
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public Integer getProjectID() {
+		return projectID;
+	}
+
+	public void setProjectID(Integer projectID) {
+		this.projectID = projectID;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public List<Tasks> getTasks() {
+		return tasks;
+	}
+
+	public void setTasks(List<Tasks> tasks) {
+		this.tasks = tasks;
+	}
+	
+	@Override
+	public String toString() {
+		return "Project [projectName=" + projectName + ", projectID=" + projectID + ", startDate=" + startDate
+				+ ", endDate=" + endDate + ", description=" + description + ", status=" + status + ", tasks=" + tasks
+				+ "]";
+	}
 }
--- calculate/src/main/java/com/vlocity/exam/model/Tasks.java
+++ calculate/src/main/java/com/vlocity/exam/model/Tasks.java
@@ -1,5 +1,76 @@
 package com.vlocity.exam.model;
 
+import java.util.Date;
+
 public class Tasks {
 
+	private String taskName;
+	private Integer taskId;
+	private Long duration;
+	private Date startDate;
+	private Date endDate;
+	private String status;
+	private Boolean isDependent;
+
+	public String getTaskName() {
+		return taskName;
+	}
+
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+
+	public Integer getTaskId() {
+		return taskId;
+	}
+
+	public void setTaskId(Integer taskId) {
+		this.taskId = taskId;
+	}
+
+	public Long getDuration() {
+		return duration;
+	}
+
+	public void setDuration(Long duration) {
+		this.duration = duration;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Boolean getIsDependent() {
+		return isDependent;
+	}
+
+	public void setIsDependent(Boolean isDependent) {
+		this.isDependent = isDependent;
+	}
+	
+	@Override
+	public String toString() {
+		return "Tasks [taskName=" + taskName + ", taskId=" + taskId + ", duration=" + duration + ", startDate="
+				+ startDate + ", endDate=" + endDate + ", status=" + status + ", isDependent=" + isDependent + "]";
+	}
 }
