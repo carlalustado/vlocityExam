68609b9ce02af6c049c2fbb1f2943de13d6f5402
Carla Lustado <carla.q.lustado@gmail.com>
2019-09-15 20:47:09 +0800

    updates

--- calculate/src/main/java/com/vlocity/exam/schedule/ScheduleProjectPlan.java
+++ calculate/src/main/java/com/vlocity/exam/schedule/ScheduleProjectPlan.java
@@ -1,6 +1,8 @@
 package com.vlocity.exam.schedule;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.collections.CollectionUtils;
@@ -23,7 +25,7 @@ public class ScheduleProjectPlan {
 			if (project.getProjectID() != null && StringUtils.isNotBlank(project.getProjectName())) {
 				if (CollectionUtils.isNotEmpty(project.getTasks())) {
 					for (Tasks task : project.getTasks()) {
-						
+						// validations and ordering
 					}
 				} else {
 					List<Tasks> tasks = new ArrayList<Tasks>();
@@ -31,12 +33,25 @@ public class ScheduleProjectPlan {
 					Tasks task = new Tasks();
 					task.setTaskId(1);
 					task.setTaskName("MOBILIZATION");
-					task.setStatus("");
+					task.setStatus("Not Started");
 					task.setIsDependent(false);
-					task.setDuration(TasksEnum.MOBILIZATION);
-					task.setEndDate(endDate);
-					task.setStartDate(startDate);
 					
+					Long duration = Long.valueOf(TasksEnum.MOBILIZATION.getValue());
+					task.setDuration(duration);
+					
+					task.setStartDate(project.getStartDate());
+
+					Calendar c = Calendar.getInstance();
+					c.setTime(project.getStartDate());
+					int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+					
+					duration = (duration / 9) - 1;
+					
+					c.add(dayOfWeek, duration.intValue());
+					
+					task.setEndDate(c.getTime());
+					
+					tasks.add(task);
 				}
 			}
 		} else {
@@ -44,4 +59,40 @@ public class ScheduleProjectPlan {
 		}
 	}
 	
+	public Project createProject(String name, Date startDate, Date endDate) {
+		
+		Project project = new Project();
+		
+		project.setProjectName(name);
+		project.setStartDate(startDate);
+		project.setEndDate(endDate);
+		
+		return project;
+	}
+	
+	public Tasks createTask(String name, String status, Boolean isDependent, Date startDate) {
+		
+		Tasks task = new Tasks();
+		task.setTaskId(1);
+		task.setTaskName(name);
+		task.setStatus(status);
+		task.setIsDependent(isDependent);
+		
+		Long duration = Long.valueOf(TasksEnum.MOBILIZATION.getValue());
+		task.setDuration(duration);
+		
+		task.setStartDate(startDate);
+
+		Calendar c = Calendar.getInstance();
+		c.setTime(startDate);
+		int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+		
+		duration = (duration / 9) - 1;
+		
+		c.add(dayOfWeek, duration.intValue());
+		
+		task.setEndDate(c.getTime());
+		
+		return task;
+	}
 }
